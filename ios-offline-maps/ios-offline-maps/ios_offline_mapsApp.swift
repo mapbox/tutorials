7ca340af69066c22514fa1578eaec290d1bb187d
Chris Whong <chris.m.whong@gmail.com>
2025-08-13 12:29:44 -0400

    add ios offline maps tutorial code

---
+++ ios-offline-maps/ios-offline-maps/ios_offline_mapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_offline_mapsApp.swift
+//  ios-offline-maps
+//
+//  Created by Chris Whong on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_offline_mapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-offline-maps/ios-offline-mapsTests/ios_offline_mapsTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_offline_mapsTests.swift
+//  ios-offline-mapsTests
+//
+//  Created by Chris Whong on 8/13/25.
+//
+
+import Testing
+@testable import ios_offline_maps
+
+struct ios_offline_mapsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
