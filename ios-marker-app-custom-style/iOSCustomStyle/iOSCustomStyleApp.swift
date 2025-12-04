8f6f035d787df6857a6e27151b091557d2c925b9
emilygamedeveloper <emily.ryan@mapbox.com>
2025-12-04 10:58:34 -0500

    Adding ios and android projects containing code for Custom Style in mobile tutorials

---
+++ ios-marker-app-custom-style/iOSCustomStyle/iOSCustomStyleApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCustomStyleApp.swift
+//  iOSCustomStyle
+//
+//  Created by Emily Ryan on 12/4/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSCustomStyleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-marker-app-custom-style/iOSCustomStyleTests/iOSCustomStyleTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSCustomStyleTests.swift
+//  iOSCustomStyleTests
+//
+//  Created by Emily Ryan on 12/4/25.
+//
+
+import Testing
+@testable import iOSCustomStyle
+
+struct iOSCustomStyleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
